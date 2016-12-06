de55613828b535e4fe6b75ec1c056f6f86a16c0b
yuliangwang <yuliangwang@Ctrip.com>
2016-12-06 09:56:22 +0800

    通过ant构建项目，并进行调试等工作。

--- src/java/main/org/apache/zookeeper/server/ZooKeeperServerMain.java
+++ src/java/main/org/apache/zookeeper/server/ZooKeeperServerMain.java
@@ -24,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 
 import javax.management.JMException;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.zookeeper.jmx.ManagedUtil;
 import org.apache.zookeeper.server.admin.AdminServer;
 import org.apache.zookeeper.server.admin.AdminServer.AdminServerException;
@@ -57,6 +58,7 @@ public class ZooKeeperServerMain {
      * @param args the configfile or the port datadir [ticktime]
      */
     public static void main(String[] args) {
+        PropertyConfigurator.configure("conf//log4j.properties");
         ZooKeeperServerMain main = new ZooKeeperServerMain();
         try {
             main.initializeAndRun(args);
